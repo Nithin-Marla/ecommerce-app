c4bcb9d13004120a7cca65151d07edc0ee4980d7
Nithin-Marla <marlanithin288@gmail.com>
2026-02-09 23:39:06 +0530

    feat: handling signup data

---
+++ src/com/stschool/ecommerceapp/controller/CustomerController.java
@@ -0,0 +1,10 @@
+package com.stschool.ecommerceapp.controller;
+
+import com.stschool.ecommerceapp.model.Customer;
+
+public class CustomerController {
+    public void handleInputSignupData(Customer customer){
+        System.out.println("Handling Customer Details: "+ customer);
+
+    }
+}
